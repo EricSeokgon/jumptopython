534446cb7af8d7dcc59528ea16e85da8d6379d3a
hadesyi <hadesleeq@gmail.com>
2016-03-03 01:23:50 +0900

    실수형

--- basic/number.py
+++ basic/number.py
@@ -1,7 +1,19 @@
-#정수형
+# 정수형
 a = 123
 print(a)
 a = -178
 print(a)
 a = 0
+print(a)
+
+# 실수형
+a = 1.2
+print(a)
+a = -3.45
+print(a)
+
+# 컴퓨터식 지수 표현 방식
+a = 4.23E10
+print(a)
+a = 4.24e-10
 print(a)
