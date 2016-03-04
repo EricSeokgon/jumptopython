2daaa8b6497469cbdd45b729d5c70a4c9fb4e910
hadesyi <hadesleeq@gmail.com>
2016-03-05 02:11:51 +0900

    문자열 자료형

---
+++ basic/string.py
@@ -0,0 +1,12 @@
+# 1.큰따옴표로 양쪽 둘러싸기
+print("Hello World")
+
+# 2.작은 따옴표로 양쪽 둘러싸기
+print('Python is fun')
+
+# 3.큰따옴표 3개를 연속으로 써서 양쪽 둘러싸기
+print("""Hello World""")
+
+# 4.작은따옴표 3개를 연속으로 써서 양쪽 둘러싸기
+print('''Hello World''')
+
