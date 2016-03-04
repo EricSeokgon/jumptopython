0139875a379143806fbfbaf80a5f8e88fc8573ee
hadesyi <hadesleeq@gmail.com>
2016-03-05 02:16:22 +0900

    문자열 자료형

--- basic/string.py
+++ basic/string.py
@@ -10,3 +10,18 @@ print("""Hello World""")
 # 4.작은따옴표 3개를 연속으로 써서 양쪽 둘러싸기
 print('''Hello World''')
 
+# 문자열에 작은따옴표 ' 포함시키기
+food = "Python's favorite food is perl"
+print(food)
+
+# 문자열에 큰땅모표 " 포함시키기
+
+say = '"Python is very easy." he says.'
+print(say)
+
+# \(백슬래시)를 이용해서 작은 따옴표 ' 와 큰따옴표"를 문자열에 포함시키기
+
+food = 'Python\'s favorite food is perl'
+say = "\"Python is very easy.\" he says."
+print(food)
+print(say)
