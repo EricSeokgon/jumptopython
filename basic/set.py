80a06e73404d02466fd92fe6973929b2835cd154
hadesyi <hadesleeq@gmail.com>
2016-10-25 10:11:11 +0900

    값 여러 개 추가하기(update)

--- basic/set.py
+++ basic/set.py
@@ -37,4 +37,10 @@ print(s2.difference(s1))
 # 집합 자료형 관련 함수들
 # 값 1개 추가하기(add)
 s1 = set([1, 2, 3])
-print(s1.add(4))
+s1.add(4)
+print(s1)
+
+# 값 여러 개 추가하기(update)
+s1 = set([1, 2, 3])
+s1.update([4, 5, 6])
+print(s1)
