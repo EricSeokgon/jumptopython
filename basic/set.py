8c12bf12e6ebfc82afe2ca1bd103d451ca0fcbec
hadesyi <hadesleeq@gmail.com>
2016-10-25 10:08:54 +0900

    3.차집합

--- basic/set.py
+++ basic/set.py
@@ -22,3 +22,14 @@ s2 = set([4, 5, 6, 7, 8, 9])
 
 # 1.교집합
 print(s1 & s2)
+print(s1.intersection(s2))
+
+# 2.합집합
+print(s1 | s2)
+print(s1.union(s2))
+
+# 3.차집합
+print(s1 - s2)
+print(s2 - s1)
+print(s1.difference(s2))
+print(s2.difference(s1))
