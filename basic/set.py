3b213039865a40d84c3769d76510da6a8b463716
hadesyi <hadesleeq@gmail.com>
2016-10-25 10:07:07 +0900

    1.교집합

--- basic/set.py
+++ basic/set.py
@@ -5,3 +5,20 @@ print(s1)
 
 s2 = set("Hello")
 print(s2)
+
+# 집합 자료형의 특징
+s1 = set([1, 2, 3])
+l1 = list(s1)
+print(l1)
+print(l1[0])
+t1 = tuple(s1)
+print(t1)
+print(t1[0])
+
+# 집합 자료형 활용하는 방법
+# 교집합, 합집합, 차집합 구하기
+s1 = set([1, 2, 3, 4, 5, 6])
+s2 = set([4, 5, 6, 7, 8, 9])
+
+# 1.교집합
+print(s1 & s2)
