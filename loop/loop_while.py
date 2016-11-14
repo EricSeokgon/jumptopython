7142418d55cf7807cb8818055238ffd62dfcd343
hadesyi <hadesleeq@gmail.com>
2016-11-14 11:13:44 +0900

    # while문 강제로 빠져나가기

--- loop/loop_while.py
+++ loop/loop_while.py
@@ -17,15 +17,7 @@ while treeHit < 10:
     if treeHit == 10:
         print("나무 넘어갑니다.")
 
-'''
-prompt = ""
-1. add
-2. del
-3. list
-4. quit
-Enter number:""
-
-'''
+# while문 직접 만들기
 prompt = """""
     1. add
     2. del
@@ -37,3 +29,15 @@ number = 0
 while number != 4:
     print(prompt)
     number = int(input())
+
+# while문 강제로 빠져나가기
+
+coffee = 10
+money = 300
+while money:
+    print("돈을 받았으니 커피를 줍니다.")
+    coffee = coffee - 1
+    print("남은 커피의 양은 %d개입니다." % coffee)
+    if not coffee:
+        print("커피가 다 떨어졌습니다. 판매를 중지합니다.")
+        break
