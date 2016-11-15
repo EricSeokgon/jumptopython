a5e2792a24bf9b32edd08582602e0037713fd994
hadesyi <hadesleeq@gmail.com>
2016-11-15 13:12:58 +0900

    # 리스트 안에 for문 포함하기

--- loop/loopFor.py
+++ loop/loopFor.py
@@ -70,3 +70,19 @@ result = []
 for num in a:
     result.append(num * 3)
 print(result)
+
+result = [num * 3 for num in a if num % 2 == 0]
+print(result)
+
+'''
+[표현식 for 항목 in 반봅가능객체 if 조건]
+
+[표현식 for 항목1 in 반복가능객체1 if 조건1
+        for 항목2 in 반복가능객체2 if 조건2
+        ...
+        for 항목n in 반곡가능객체n if 조건n]
+'''
+
+result = [x*y for x in range(2,10)
+        for y in range(1,10)]
+print(result)
