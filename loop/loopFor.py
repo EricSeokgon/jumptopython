dc933c65c815045457134c2194d2fdabb93774ec
hadesyi <hadesleeq@gmail.com>
2016-11-15 13:07:58 +0900

    # for와 range를 이용한 구구단

--- loop/loopFor.py
+++ loop/loopFor.py
@@ -57,3 +57,9 @@ for i in range(1, 11):
 for number in range(len(marks)):
     if marks[number] < 60: continue
 print("%d번 학생 축하합니다. 합격입니다." % (number + 1))
+
+# for와 range를 이용한 구구단
+for i in range(2, 10):
+    for j in range(1, 10):
+        print(i * j, end=" ")
+    print(" ")
