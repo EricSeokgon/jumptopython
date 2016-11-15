5f0305268fcbf76ea9142fffaccee86201c042bc
hadesyi <hadesleeq@gmail.com>
2016-11-15 10:16:05 +0900

    1.전형적인 for문

---
+++ loop/loopFor.py
@@ -0,0 +1,15 @@
+# 03-3 for문
+
+# for문의 기본 구조
+
+'''
+
+for 변수 in 리스트(또는 튜플, 문자열)
+    수행할 문장1
+    수행할 문장2
+'''
+
+# 1.전형적인 for문
+test_list = ['one', 'two', 'three']
+for i in test_list:
+    print(i)
