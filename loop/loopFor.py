baa72e5d6a8cc9b4d2808d9f1ab18ad835ae9577
hadesyi <hadesleeq@gmail.com>
2016-11-15 10:18:59 +0900

    # 2.다양한 for문의 사용

--- loop/loopFor.py
+++ loop/loopFor.py
@@ -13,3 +13,8 @@ for 변수 in 리스트(또는 튜플, 문자열)
 test_list = ['one', 'two', 'three']
 for i in test_list:
     print(i)
+
+# 2.다양한 for문의 사용
+a = [(1, 2), (3, 4), (5, 6)]
+for (first, last) in a:
+    print(first + last)
