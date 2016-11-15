ea5fb12b51d11d9b2b49c95c611f7e969d2acf7c
hadesyi <hadesleeq@gmail.com>
2016-11-15 13:06:17 +0900

    # range 함수의 예시 살펴보기

--- loop/loopFor.py
+++ loop/loopFor.py
@@ -53,3 +53,7 @@ sum = 0
 for i in range(1, 11):
     sum = sum + i
     print(sum)
+
+for number in range(len(marks)):
+    if marks[number] < 60: continue
+print("%d번 학생 축하합니다. 합격입니다." % (number + 1))
