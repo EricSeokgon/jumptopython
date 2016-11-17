64e30637653f6e1bf408b8e0f2f118d1f30bcad0
hadesyi <hadesleeq@gmail.com>
2016-11-17 13:29:42 +0900

    일반적인 함수

--- inout/function.py
+++ inout/function.py
@@ -16,3 +16,22 @@ a = 3
 b = 4
 c = sum(a, b)
 print(c)
+
+# 입력값과 결과값에 따른 함수의 형태
+
+# 일반적인 함수
+'''
+def 함수이름(입력인수):
+    <수행할 문장>
+    ...
+    return 결과값
+'''
+
+
+def sum(a, b):
+    result = a + b
+    return result
+
+
+a = sum(3, 4)
+print(a)
