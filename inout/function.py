bfe9157f00a6480a75d3107293b8cb1f95ea2d37
hadesyi <hadesleeq@gmail.com>
2016-11-21 14:06:19 +0900

    #입력 인수에 초기값 미리 설정하기

--- inout/function.py
+++ inout/function.py
@@ -132,4 +132,11 @@ def sum_and_mul(a, b):
     return a + b
 
 
-#입력 인수에 초기값 미리 설정하기
+# 입력 인수에 초기값 미리 설정하기
+def say_myself(name, old, man=True):
+    print("나의 이름은  %s 입니다." % name)
+    print("나이는 %d살입니다." % old)
+    if man:
+        print("남자입니다.")
+    else:
+        print("여자입니다.")
